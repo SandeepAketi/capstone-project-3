47cbdab556dc91e7b6f327c2d80480c4584356f8
SandeepAketi <sandeepaketi537@gmail.com>
2023-09-27 13:44:18 +0530

    first commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Software Developer",
+          "Full stack developer",
+          "Frontend developer"
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
